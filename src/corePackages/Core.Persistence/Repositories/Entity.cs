6c9069446ae6564579468d421d36877630d5d065
engin can <engin_can1528@hotmail.com>
2022-12-10 22:09:01 +0300

    default proje yapısı oluşturuldu

--- src/corePackages/Core.Persistence/Repositories/Entity.cs
+++ src/corePackages/Core.Persistence/Repositories/Entity.cs
@@ -1,15 +1,54 @@
 ﻿namespace Core.Persistence.Repositories;
 
+/// <summary>
+/// Base entity
+/// </summary>
 public class Entity
 {
-    public int Id { get; set; }
+    /// <summary>
+    /// Verinin durum bilgisidir.
+    /// </summary>
+    public DataStatus DataStatus { get; set; }
+
+    /// <summary>
+    /// Kaydın oluşturan kullanıcı bilgisidir.
+    /// </summary>
+    public int? CreatedUserId { get; set; }
+
+    /// <summary>
+    /// Kaydın oluşturulma tarih bilgisidir.
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
 
     public Entity()
     {
     }
 
-    public Entity(int id) : this()
+    public Entity(DataStatus dataStatus, int? createdUserId, DateTime createdAt) : this()
     {
-        Id = id;
+        DataStatus = dataStatus;
+        CreatedUserId = createdUserId;
+        CreatedAt = createdAt;
     }
+}
+
+/// <summary>
+/// Verilerin durum bilgisini tutan alandır. Sistem üzerinde hiçbir veri silinmez
+/// </summary>
+public enum DataStatus
+{
+    /// <summary>
+    /// Verilerin silinme durum bilgisidir.
+    /// </summary>
+    Deleted = 1,
+
+    /// <summary>
+    /// Verilen aktiflik durum bilgisidir.
+    /// </summary>
+    Activated,
+
+    /// <summary>
+    /// Veriyi pasife alınıp/alınmadığının bilgisidir.
+    /// </summary>
+    DeActivated
 }
---
+++ src/defaultProject/Application/Entities/Users/User.cs
@@ -0,0 +1,63 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Entities.Users
+{
+    /// <summary>
+    /// Tüm kullanıcıların tutulduğu tablodur.
+    /// </summary>
+    public class User : Entity
+    {
+        public User()
+        {
+        }
+
+        public User(int id, string name, string surname, string fullName, string userName, string password, DataStatus dataStatus, int? createdUserId, DateTime createdAt):this()
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            FullName = fullName;
+            UserName = userName;
+            Password = password;
+            DataStatus = dataStatus;
+            CreatedUserId = createdUserId;
+            CreatedAt = createdAt;
+        }
+
+        /// <summary>
+        /// Tablo tekil bilgisidir.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Kullanıcı isim bilgisinin tutulduğu alandır
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Kullanıcı soyisim bilgisinin tutulduğu alandır
+        /// </summary>
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// İsim ve soyisim bilgisinin toplu tutulduğu alandır
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Kullanıcı ad bilgisidir.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Kullanıcı şifre bilgisidir.
+        /// </summary>
+        public string Password { get; set; }
+
+    }
+}
