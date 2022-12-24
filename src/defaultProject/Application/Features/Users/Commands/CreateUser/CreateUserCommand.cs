a849ea454ff1182f667ed4972a9fe084c99d6b2d
engin can <engin_can1528@hotmail.com>
2022-12-25 00:23:17 +0300

    .

--- src/defaultProject/Application/Features/Users/Commands/CreateUser/CreateUserCommand.cs
+++ src/defaultProject/Application/Features/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -11,6 +11,16 @@ namespace Application.Features.Users.Commands.CreateUser
     {
         public string Name { get; set; }
 
+        public string Surname { get; set; }
+
+        public string FullName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
         public class CreateBrandCommandHandler : IRequestHandler<CreateUserCommand, CreateUserDto>
         {
             private readonly IUserRepository _brandRepository;
